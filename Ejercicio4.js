dce99f8c6c440fa054afe70076a383646b249b3f
Eli <113862594+Elizbeth99@users.noreply.github.com>
2022-11-24 11:46:38 -0600

    Add files via upload

---
+++ Ejercicio4.js
@@ -0,0 +1,17 @@
+const nombre= "Elizbeth"
+const apell= "Romero"
+
+const estudiante= nombre.concat("").concat(apell)
+
+const estudianteMayus= estudiante.toUpperCase()
+const estudianteMinus= estudiante.toLowerCase()
+
+const estudianteLength= estudiante.length
+
+const inicialnombre= nombre.substring(0,1)
+
+const finalapell= apell.substring(apell.length-1, apell.length)
+
+const estudiantesinespacio= estudiante.replace(//g,"")
+
+const nombreEnEstudiante = estudiante.includes(nombre)
